946bacba6d11ea36ff681328c6dfc734e1e3b288
Pablo Angelani <pablo.angelani@gmail.com>
2016-02-14 23:48:57 -0300

    new data structure

--- db_data.sql
+++ db_data.sql
@@ -1,6 +1,3 @@
-INSERT INTO sex(id, name) VALUES (1, 'male');
-INSERT INTO sex(id, name) VALUES (2, 'female');
-
 INSERT INTO statcategory(id, name) VALUES (1, 'agility');
 INSERT INTO statcategory(id, name) VALUES (2, 'arc');
 INSERT INTO statcategory(id, name) VALUES (3, 'attack');
--- db_dump.sql
+++ db_dump.sql
@@ -1,61 +1,81 @@
-CREATE TABLE sex (
-    id int(11) NOT NULL AUTO_INCREMENT,
-    name varchar(255) NOT NULL
-}
-
 CREATE TABLE user (
     id int(11) NOT NULL AUTO_INCREMENT,
     username varchar(255) NOT NULL,
     fullname varchar(255) NOT NULL,
     date_of_birth DATE NOT NULL,
-    sex_id int(11) NOT NULL,
-    weigth FLOAT NOT NULL,
+    male int(1) NOT NULL DEFAULT '1',
+    weight FLOAT NOT NULL,
+    heigth FLOAT NOT NULL,
     PRIMARY KEY (id),
-    UNIQUE(username),
-    CONSTRAINT FK_appUser_sex_id FOREIGN KEY (sex_id) REFERENCES sex(id)
+    UNIQUE(username)
 );
 
 CREATE TABLE teambench (
-    id int NOT NULL AUTO_INCREMENT,
+    id int(11) NOT NULL AUTO_INCREMENT,
+    user_id_creator int(11) NOT NULL,
     name varchar(255) NOT NULL,
-    PRIMARY KEY (id)
+    creation_date DATETIME NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT FK_teambench_user_id FOREIGN KEY (user_id_creator) REFERENCES user(id)
 );
 
 CREATE TABLE r_teambench_user (
-    teambench_id int NOT NULL AUTO_INCREMENT,
-    user_id varchar(255) NOT NULL,
-    PRIMARY KEY (team_id, user_id),
-    CONSTRAINT FK_r_teambench_user_team_id FOREIGN KEY (team_id) REFERENCES team(id),
+    teambench_id int(11) NOT NULL,
+    user_id int(11) NOT NULL,
+    PRIMARY KEY (teambench_id, user_id),
+    CONSTRAINT FK_r_teambench_user_teambench_id FOREIGN KEY (teambench_id) REFERENCES teambench(id),
     CONSTRAINT FK_r_teambench_user_user_id FOREIGN KEY (user_id) REFERENCES user(id)
 );
 
 CREATE TABLE team (
-    id int NOT NULL AUTO_INCREMENT,
+    id int(11) NOT NULL AUTO_INCREMENT,
     name varchar(255) NOT NULL,
-    teambench_id NOT NULL,
+    user_id_creator int(11) NOT NULL,
+    teambench_id int(11) NOT NULL,
+    creation_date DATETIME NOT NULL,
     PRIMARY KEY (id),
-    CONSTRAINT FK_team_teambench_id FOREIGN KEY (teambench_id) REFERENCES teambench(id)
+    CONSTRAINT FK_team_teambench_id FOREIGN KEY (teambench_id) REFERENCES teambench(id),
+    CONSTRAINT FK_team_user_id_creator FOREIGN KEY (user_id_creator) REFERENCES user(id)
 );
 
 CREATE TABLE r_team_user (
-    team_id int NOT NULL AUTO_INCREMENT,
-    user_id varchar(255) NOT NULL,
+    team_id int(11) NOT NULL,
+    user_id int(11) NOT NULL,
     PRIMARY KEY (team_id, user_id),
     CONSTRAINT FK_r_team_user_team_id FOREIGN KEY (team_id) REFERENCES team(id),
     CONSTRAINT FK_r_team_user_user_id FOREIGN KEY (user_id) REFERENCES user(id)
 );
 
-CREATE TABLE match (
-    id int NOT NULL AUTO_INCREMENT,
-    date DATE NOT NULL,
+CREATE TABLE `match` (
+    id int(11) NOT NULL AUTO_INCREMENT,
+    date DATETIME NOT NULL,
     place varchar(1024) NOT NULL,
-    team_id_a int NOT NULL,
-    team_id_b int NOT NULL,
-    team_id_winner int NOT NULL,
-    score varchar(255) NOT NULL,
+    teambench_id int NOT NULL,
+    team_id_a int(11) NOT NULL,
+    team_id_b int(11) NOT NULL,
     PRIMARY KEY (id),
-    CONSTRAINT FK_history_team_id_a FOREIGN KEY (team_id_a) REFERENCES team(id),
-    CONSTRAINT FK_history_team_id_b FOREIGN KEY (team_id_b) REFERENCES team(id)
+    CONSTRAINT FK_match_teambench_id FOREIGN KEY (teambench_id) REFERENCES teambench(id),
+    CONSTRAINT FK_match_team_id_a FOREIGN KEY (team_id_a) REFERENCES team(id),
+    CONSTRAINT FK_match_team_id_b FOREIGN KEY (team_id_b) REFERENCES team(id)
+);
+
+CREATE TABLE matchresult (
+    match_id int(11) NOT NULL,
+    team_id_winner int(11) NOT NULL,
+    score_winner int NOT NULL,
+    score_looser int NOT NULL,
+    PRIMARY KEY (match_id),
+    CONSTRAINT FK_matchresult_match_id FOREIGN KEY (match_id) REFERENCES `match`(id),
+    CONSTRAINT FK_matchresult_team_id_winner FOREIGN KEY (team_id_winner) REFERENCES team(id)
+);
+
+CREATE TABLE userscore (
+    match_id int(11) NOT NULL,
+    user_id int(11) NOT NULL,
+    scores int NOT NULL DEFAULT 0,
+    PRIMARY KEY (match_id, user_id),
+    CONSTRAINT FK_userscore_match_id FOREIGN KEY (match_id) REFERENCES `match`(id),
+    CONSTRAINT FK_userscore_user_id FOREIGN KEY (user_id) REFERENCES user(id)
 );
 
 CREATE TABLE statcategory (
@@ -64,15 +84,15 @@ CREATE TABLE statcategory (
     PRIMARY KEY (id)
 );
 
-CREATE TABLE statscore (
+CREATE TABLE user_match_stat (
     user_id int NOT NULL,
     user_id_subject int NOT NULL,
     statcategory_id int NOT NULL,
     match_id int NOT NULL,
     score int NOT NULL,
     PRIMARY KEY (user_id, user_id_subject, statcategory_id, match_id),
-    CONSTRAINT FK_statscore_user_id FOREIGN KEY (user_id) REFERENCES user(id),
-    CONSTRAINT FK_statscore_user_id_subject FOREIGN KEY (user_id_subject) REFERENCES user(id),
-    CONSTRAINT FK_statscore_statcategory_id FOREIGN KEY (statcategory_id) REFERENCES statcategory(id),
-    CONSTRAINT FK_statscore_match_id FOREIGN KEY (match_id) REFERENCES match(id)
+    CONSTRAINT FK_user_match_stat_user_id FOREIGN KEY (user_id) REFERENCES user(id),
+    CONSTRAINT FK_user_match_stat_user_id_subject FOREIGN KEY (user_id_subject) REFERENCES user(id),
+    CONSTRAINT FK_user_match_stat_statcategory_id FOREIGN KEY (statcategory_id) REFERENCES statcategory(id),
+    CONSTRAINT FK_user_match_stat_match_id FOREIGN KEY (match_id) REFERENCES `match`(id)
 );
